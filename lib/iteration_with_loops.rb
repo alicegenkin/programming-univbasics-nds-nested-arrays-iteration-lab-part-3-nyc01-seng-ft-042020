7c1cd80467757aaec6566ad5059c91bfa233a20d
Alice <agenkin1@gmail.com>
2020-04-06 19:01:27 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,6 @@
 def join_nested_strings(src)
   newstr = ''
-  src.each do |inner_src| 
+  src.each do |inner_src|
     inner_src.each do |inner_inner_src|
       if inner_inner_src.is_a?String
         newstr = newstr +inner_inner_src + " "
